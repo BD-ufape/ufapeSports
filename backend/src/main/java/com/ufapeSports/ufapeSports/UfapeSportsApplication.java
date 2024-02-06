75fb25677f17142e6d5984ae4e7dccb77175a048
JoasGomes <jojoca.vitor777@gmail.com>
2024-02-06 17:01:57 -0300

    adicionando o back e front

---
+++ backend/src/main/java/com/ufapeSports/ufapeSports/UfapeSportsApplication.java
@@ -0,0 +1,13 @@
+package com.ufapeSports.ufapeSports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfapeSportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfapeSportsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ufapeSports/ufapeSports/UfapeSportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufapeSports.ufapeSports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfapeSportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
