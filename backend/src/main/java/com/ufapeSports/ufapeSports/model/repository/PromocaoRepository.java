cbb33c10c3b18fa2b472c3f1d36f4f443087f3da
leonardonb <leonardonb@gmail.com>
2024-02-12 00:12:03 -0300

    Implementação Services, Controler e Repositorio de Promoção

--- backend/src/main/java/com/ufapeSports/ufapeSports/model/repository/PromocaoRepository.java
+++ backend/src/main/java/com/ufapeSports/ufapeSports/model/repository/PromocaoRepository.java
@@ -1,9 +1,18 @@
 package com.ufapeSports.ufapeSports.model.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ufapeSports.ufapeSports.model.entity.Promocao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDateTime;
+import java.util.List;
 
-public interface PromocaoRepository extends JpaRepository<Promocao, Long>{
+@Repository
+public interface PromocaoRepository extends JpaRepository<Promocao, Long> {
+    List<Promocao> findByDataInicioBeforeAndDataFimAfter(LocalDateTime dataInicio, LocalDateTime dataFim);
 
+    List<Promocao> findByDescricaoContainingIgnoreCase(String descricao);
+    
+    List<Promocao> findByDataInicioBeforeAndDataFimAfterOrDataFimIsNull(LocalDateTime dataAtual, LocalDateTime dataAtual2);
+
+    List<Promocao> findByPorcentagemGreaterThan(float porcentagem);
 }
